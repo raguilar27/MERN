9af847916d4b6964d898fb53f952654f5fa1155e
Rogelio Aguilar <raguilar27@github.com>
2021-09-23 00:36:20 -0700

    created get, post, delete routes for items

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -14,6 +15,9 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+//Use Routes
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
