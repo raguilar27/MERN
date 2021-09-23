7771744ef5f14ae6a58fe9d3098943664bd6496a
Rogelio Aguilar <raguilar27@github.com>
2021-09-22 23:32:45 -0700

    connected to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://raguilar27:Rogelio209@mernfundamentals.6unlu.mongodb.net/ShoppingList?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
