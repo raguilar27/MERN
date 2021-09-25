3adc32f84c675d1e623497ed492e776fe22bfe15
Rogelio Aguilar <raguilar27@github.com>
2021-09-24 17:10:38 -0700

    created shopping list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppNavBar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <AppNavBar />
+      <ShoppingList />
     </div>
   );
 }
