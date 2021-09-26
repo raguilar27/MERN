60b5b7df27ebbe01f92e729217b140da4c4e5a6a
Rogelio Aguilar <raguilar27@github.com>
2021-09-25 19:00:05 -0700

    set up boilerplaye for get items using redux

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,22 @@
+import { v4 as uuid } from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+
+const initialState = {
+  items: [
+    { id: uuid(), name: "Chicken" },
+    { id: uuid(), name: "Rice" },
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Steak" },
+  ],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
