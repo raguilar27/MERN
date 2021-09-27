2c4db2600539ab6148318d2c58b1af78bc5256bf
Rogelio Aguilar <raguilar27@github.com>
2021-09-26 21:10:55 -0700

    created an route that gets user info

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 //Item Model
 const Item = require("../../models/Item");
 
 //@route GET api/items
 //@desc Get all items
-//@access Public
+//@access Private
 router.get("/", (req, res) => {
   Item.find()
     .sort({ date: -1 })
@@ -16,7 +17,7 @@ router.get("/", (req, res) => {
 //@route POST api/items
 //@desc Create an item
 //@access Public
-router.post("/", (req, res) => {
+router.post("/", auth, (req, res) => {
   const newItem = new Item({
     name: req.body.name,
   });
@@ -26,8 +27,8 @@ router.post("/", (req, res) => {
 
 //@route DELETE api/items
 //@desc Delete an item
-//@access Public
-router.delete("/:id", (req, res) => {
+//@access Private
+router.delete("/:id", auth, (req, res) => {
   Item.findById(req.params.id)
     .then((item) => item.remove().then(() => res.json({ success: true })))
     .catch((err) => res.status(404).json({ success: false }));
