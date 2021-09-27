369fe4c1c5e0b497fbdbceb26d6b0278ea11d3b8
Rogelio Aguilar <raguilar27@github.com>
2021-09-27 00:05:29 -0700

    redux checks for token

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import React, { Component } from "react";
 import AppNavBar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
 import ItemModal from "./components/itemModal";
@@ -7,19 +8,26 @@ import { Container } from "reactstrap";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavBar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -9,14 +9,14 @@ import {
   REGISTER_FAIL,
 } from "../actions/types";
 
-const intialState = {
+const initialState = {
   token: localStorage.getItem("token"),
   isAuthenticated: null,
   isLoading: false,
   user: null,
 };
 
-export default function (state = intialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case USER_LOADING:
       return {
@@ -42,6 +42,7 @@ export default function (state = intialState, action) {
     case LOGIN_FAIL:
     case LOGOUT_SUCCESS:
     case REGISTER_FAIL:
+      localStorage.removeItem("token");
       return {
         ...state,
         token: null,
